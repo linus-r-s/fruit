0d29ffbfc87fe4fa045d44a9bc137d7f977f67eb
linus-r-s <31243041+linus-r-s@users.noreply.github.com>
2022-02-02 10:58:04 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+let player: game.LedSprite = null
+player = game.createSprite(2, 4)
+let fruit = game.createSprite(randint(0, 4), 0)
+let speed = 500
+basic.forever(function () {
+    fruit.change(LedSpriteProperty.Y, 1)
+    basic.pause(speed)
+    if (fruit.get(LedSpriteProperty.Y) == 4) {
+        if (fruit.get(LedSpriteProperty.X) == player.get(LedSpriteProperty.X)) {
+            game.addScore(1)
+            speed += -10
+            fruit.set(LedSpriteProperty.Y, 0)
+            fruit.set(LedSpriteProperty.X, randint(0, 4))
+        } else {
+            game.gameOver()
+        }
+    }
+})
