faf14a0512e00b392372e29f86c46f6e0f51fdb5
linus-r-s <31243041+linus-r-s@users.noreply.github.com>
2022-02-09 10:48:30 +0100

    adds lives

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ let player: game.LedSprite = null
 player = game.createSprite(2, 4)
 let fruit = game.createSprite(randint(0, 4), 0)
 let speed = 500
+game.setLife(3)
 basic.forever(function () {
     fruit.change(LedSpriteProperty.Y, 1)
     basic.pause(speed)
@@ -16,7 +17,8 @@ basic.forever(function () {
             game.addScore(1)
             speed += -10
         } else {
-            game.gameOver()
+            speed = 500
+            game.removeLife(1)
         }
         fruit.set(LedSpriteProperty.Y, 0)
         fruit.set(LedSpriteProperty.X, randint(0, 4))
